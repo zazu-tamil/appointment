146493c69e06d5a0b283972485bac01b9d4be83d
zazu-tamil <crinfotechcbe@gmail.com>
2025-11-10 16:04:44 +0530

    init

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "db";
+$username = "root";  
+$password = ""; 
+$dbname = "edoc"; 
+
+$database = new mysqli($servername, $username, $password, $dbname);
+
+if ($database->connect_error) {
+    die("Échec de la connexion : " . $database->connect_error);
+}
+?>
+
